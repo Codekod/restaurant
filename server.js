c1e81f912f37fdacb0866adb4f11d158a0fd1afd
Melih EKEN <meliheken@aof.anadolu.edu.tr>
2025-08-17 21:41:29 +0300

    404 Hatası Düzelt

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Database connection
